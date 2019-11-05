a806b6f336013c1423f5e24dd2bcaae7efcaaf8a
William Zhang <william@stackadapt.com>
2019-11-05 15:11:56 -0500

    initial commit

---
+++ trebuchet.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+const char *msg = R"V0G0N(MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWOo;.  ,oXMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkdc,.        .OMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKko:,.               ,WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXkl;.                     'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXOdc;.                          'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkoc::;;:coxKWMMMMMMMMMMMMMMMWXOdc,.                                'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWOl'             .:kWMMMMMMMMNOo;.                          .:'         'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk,                   .dNNKxl;'.                        .,cdOXWMo         'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0;                       .                         .;cdONMMMMMMMMo         'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd                                              .;lkXMMMMMMMMMMMMMMo         'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0.          .;clc,.                       .,cokXWMMMMMMMMMMMMMMMMMMo         'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:          dWMMMMMWO.               .,cokXWMMMMMMMMMMMMMMMMMMMMMMMMo         'WMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW'         lMMMMMMMMMx          .;cxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo         'WMMMMMMMMM
+Nd:'..................:oKMMMMMMMKko;          lMMMMMMMMMx          OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXk,         .dKWMMMMMMM
+;                       .OMXOd:.               oNMMMMMNk.         'NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0c'               .;xNMMMM
+                         .,                     .;ldl;.           dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNc                     .0MMM
+                                                                 :WMMMMMMMMMMMMMMMMMMMMMMMMMMMWk.                       .:KM
+                                                               .kWMMMMMMMMMMMMMMMMMMMMMMMMMMMWc                           .K
+          kXXXX;                   .;dl.                        lNMMMMMMMMMMMMMMMMMMMMMMMMMMMk           ,d0XKkc.          :
+          0MMMM:               'cdONWo                           ;NMMMMMMMMMMMMMMMMMMMMMMMMMM:         .OMMMMMMMX;         .
+          0MMMM:          .:okXMMMMWl          .lc:::co'          ;NMMMMMMMMMMMMMMMMMMMMMMMMW'         lMMMMMMMMMO          
+          0MMMM:         ,WMMMMMMMMd          .0MMMMMMMX'          :WMMMMMMMMMMMMMMMMMMMMMMMM,         ,WMMMMMMMMd          
+          0MMMM:         ,MMMMMMMMx          .kMMMMMMMMMK.          cNMMMMMMMMMMMMMMMMMMMMMMMo          ;kXMMMN0o.         ,
+          .,,,,.         ,MMMMMMX:          .OMMMMMMMMMMMK'          .OMMMMMMMMMMMMMMMMMMMMMMN,            .''.            O
+                         ,MMMMMN,          .kMMMMMMMMMMMMMK,          .0MMMMMMMMMMMMMMMMMMMMMMK,                          dM
+                         ;MMMMN;          ,KMMMMMMMMMMMMMMMNc          .KMMMMMMMMMMMMMMMMMMMMMMWx.                      cKMM
+.                        dMMMN,          ,XMMMMMMMMMMMMMMMMMWl          .KMMMMMMMMMMMMMMMMMMMMMMMNo.                 .c0MMMM
+0'                     .oWMMN:          'XMMMMMMMMMMMMMMMMMMMW:          .0MMMMMMMMMMMMMMMMMMMMMMMMWKd;.          'lkNMMMMMM
+MWKkdddddddddddddddddx0WMMM0'          .KMMMMMMMMMMMMMMMMMMMMMN;          .xMMMMMMMMMMMMMMMMMMMMMMMMMMMN0Oxdddxk0XWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMO.          :XMMMMMMMMMMMMMMMMMMMMMMMNd.          dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMM0.          lWMMMMMMMMMMMMMMMMMMMMMMMMMMk           xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMK.          cWMMMMMMMMMMMMMMMMMMMMMMMMMMMMx          .kMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMWk.          lWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMk           kMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWc           oWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMk.          cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMo          .kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK,          ;NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMd          .OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX'          :NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMWo          .0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX,          ;NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMX:          .OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX.          :WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MN0xooooooooooooo'           'ooooooooooooooooooooooooooooooooooooooooooo;           ,ooooooooooooodOXMMMMMMMMMMMMMMMMMMMMMM
+x.                                                                                                    lNMMMMMMMMMMMMMMMMMMMM
+.                                                                                                      xMMMMMMMMMMMMMMMMMMMM
+                                                                                                       oMMMMMMMMMMMMMMMMMMMM
+                                                                                                       oMMMMMMMMMMMMMMMMMMMM
+          ,::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.         ,xxxxxxxxxxxxxxxxONMM
+          0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW'                           .,0
+          0MMMMMMMMMMMMMMWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWMMMMMMMMMMMMMW'                             .
+          0MMMMMMMWKdc,'...',:oONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0dc;'...',:d0WMMMMMMMW'                              
+          0MMMMNx;.             .,lKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXd;.             .;oXMMMMW'                              
+          0MMNo.                    ;0MMMMMMMMMMMMMMMMMMMMMMMMMMMXl.                    :KMMW'         .:::::::::,          
+          0M0'                       .lXMMMMMMMMMMMMMMMMMMMMMMMWk.                       .dNW'         oMMMMMMMMM0          
+          0d                           ,NMMMMMMMMMMMMMMMMMMMMMWl                           :N'         oMMMMMMMMM0          
+          .           ;oxOko:.          .,,,,,,,,,,,,,,,,,,,,,'           ,lxOko;.          .          oMMMMMMMMMK.         
+                    .kMMMMMMMX,                                         .0MMMMMMMK.                    oMMMMMMMMMMo        c
+                    cMMMMMMMMMk                                         oMMMMMMMMMd                    oMMMMMMMMMMWOl;'';ckW
+.                   ;WMMMMMMMMd                                         cWMMMMMMMMo                    kMMMMMMMMMMMMMMMMMMMM
+0.                   :XMMMMMNx.                                          cKWMMMMNo                   .xMMMMMMMMMMMMMMMMMMMMM
+MW0kddddddo.          .':c:,.           :dddddddddddddddddddddl.           ':c:'.           cddddddxONMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMM0'                          oWMMMMMMMMMMMMMMMMMMMMMMk.                         .xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMWo                       ,KMMMMMMMMMMMMMMMMMMMMMMMMMNl                       :XMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMM0:                   'xWMMMMMMMMMMMMMMMMMMMMMMMMMMMWO;                   ,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMXdc,.          .:d0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKxc'.          'cxKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMXo,.   .,cOWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKl;.   .,l0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+
+
+									---- by the great siege (and data) engineer William)V0G0N";
+	cout << msg << endl;
+	return 0;
+}
